e6b9e3554c53f23776eea908415620aeb6bc678e
Lionel Verrier <lgv3lee@gmail.com>
2022-12-23 18:12:42 -0500

    register user complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,11 +25,12 @@ if(userExists){
 const salt = await bcrypt.genSalt(10);
 const hashedPassword = await bcrypt.hash(password, salt);
 
+
 //Create user
 const user = await User.create({
   name,
   email,
-  password:hashedPassword
+  password: hashedPassword
 })
 
 if(user){
