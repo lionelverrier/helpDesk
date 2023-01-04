fda80c6efd1f42ca2728357551ad8f9a1a721830
Lionel Verrier <lgv3lee@gmail.com>
2023-01-04 10:38:28 -0500

    project complete minus deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
@@ -23,5 +24,18 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production'){
+  //set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req,res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+  app.get('/', (req, res) => {
+    res.json({message: 'Welcome to the support desk API'});
+  });
+}
 
 app.use(errorHandler);
